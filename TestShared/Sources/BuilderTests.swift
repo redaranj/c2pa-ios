17ddedd6e2f2340b910ffbd742c8336ee36f4421
Darren Clarke <darren@redaranj.com>
2025-12-18 10:02:15 +0100

    Fix BuilderTests to use PR #26 Stream API

--- TestShared/Sources/BuilderTests.swift
+++ TestShared/Sources/BuilderTests.swift
@@ -1,10 +1,10 @@
-// This file is licensed to you under the Apache License, Version 2.0 
-// (http://www.apache.org/licenses/LICENSE-2.0) or the MIT license 
+// This file is licensed to you under the Apache License, Version 2.0
+// (http://www.apache.org/licenses/LICENSE-2.0) or the MIT license
 // (http://opensource.org/licenses/MIT), at your option.
 //
-// Unless required by applicable law or agreed to in writing, this software is 
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS OF 
-// ANY KIND, either express or implied. See the LICENSE-MIT and LICENSE-APACHE 
+// Unless required by applicable law or agreed to in writing, this software is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS OF
+// ANY KIND, either express or implied. See the LICENSE-MIT and LICENSE-APACHE
 // files for the specific language governing permissions and limitations under
 // each license.
 
@@ -292,7 +292,7 @@ public final class BuilderTests: TestImplementation {
                 try? FileManager.default.removeItem(at: archiveFile)
             }
 
-            let archiveStream = try Stream(fileURL: archiveFile, truncate: true, createIfNeeded: true)
+            let archiveStream = try Stream(writeTo: archiveFile)
             try builder.writeArchive(to: archiveStream)
 
             let fileExists = FileManager.default.fileExists(atPath: archiveFile.path)
@@ -324,7 +324,7 @@ public final class BuilderTests: TestImplementation {
                 try? FileManager.default.removeItem(at: archiveFile)
             }
 
-            let archiveStream = try Stream(fileURL: archiveFile, truncate: true, createIfNeeded: true)
+            let archiveStream = try Stream(writeTo: archiveFile)
             try builder.writeArchive(to: archiveStream)
 
             let fileExists = FileManager.default.fileExists(atPath: archiveFile.path)
@@ -356,7 +356,7 @@ public final class BuilderTests: TestImplementation {
                 try? FileManager.default.removeItem(at: archiveFile)
             }
 
-            let archiveStream = try Stream(fileURL: archiveFile, truncate: true, createIfNeeded: true)
+            let archiveStream = try Stream(writeTo: archiveFile)
             try builder.writeArchive(to: archiveStream)
 
             let fileExists = FileManager.default.fileExists(atPath: archiveFile.path)
