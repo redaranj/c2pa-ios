ce317b744cbe80da713112d9b94327ed30735c24
Darren Clarke <darren@redaranj.com>
2025-12-19 08:47:29 +0100

    Update copyright headers

--- Library/Sources/Manifest/ClaimGeneratorInfo.swift
+++ Library/Sources/Manifest/ClaimGeneratorInfo.swift
@@ -1,8 +1,14 @@
+// This file is licensed to you under the Apache License, Version 2.0
+// (http://www.apache.org/licenses/LICENSE-2.0) or the MIT license
+// (http://opensource.org/licenses/MIT), at your option.
+//
+// Unless required by applicable law or agreed to in writing, this software is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS OF
+// ANY KIND, either express or implied. See the LICENSE-MIT and LICENSE-APACHE
+// files for the specific language governing permissions and limitations under
+// each license.
 //
 //  ClaimGeneratorInfo.swift
-//  C2PA
-//
-//  Created by Benjamin Erhart on 06.10.25.
 //
 
 import Foundation
--- TestShared/Sources/ManifestTests.swift
+++ TestShared/Sources/ManifestTests.swift
@@ -1,10 +1,16 @@
-import C2PA
-import Foundation
+// This file is licensed to you under the Apache License, Version 2.0
+// (http://www.apache.org/licenses/LICENSE-2.0) or the MIT license
+// (http://opensource.org/licenses/MIT), at your option.
+//
+// Unless required by applicable law or agreed to in writing, this software is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS OF
+// ANY KIND, either express or implied. See the LICENSE-MIT and LICENSE-APACHE
+// files for the specific language governing permissions and limitations under
+// each license.
+//
+//  ManifestTests.swift
+//
 
-// Manifest tests - pure Swift implementation
-public final class ManifestTests: TestImplementation {
-
-    public init() {}
 
     public func testMinimal() -> TestResult {
         let manifest = ManifestDefinition(claimGeneratorInfo: [], title: "test")
